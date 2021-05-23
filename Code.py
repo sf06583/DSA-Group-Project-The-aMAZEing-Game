0f1be08f9b9e3097d01b92b6d96c43081a1a2178
Mustafasohail7 <66203849+Mustafasohail7@users.noreply.github.com>
2021-05-23 19:01:58 +0500

    Updated code.py

--- Code.py
+++ Code.py
@@ -112,3 +112,64 @@ visited=[]
 lives=3
 font_details=pygame.font.Font(None,32)
 back_track =font_details.render("DON'T BAACKTRACK",True,(0,0,0))#takes three parameters, text, antialias, colour 
+
+         
+        
+        
+        
+    
+# if len(visited)==len(path):
+#     lives+=1
+dash=pygame.image.load('youwin.jpg')
+you_lose=pygame.image.load('youlose.jpg')
+back_track=pygame.image.load('back_track.png')
+i=lev_1[0]
+quitt=False
+count=0
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            quit()
+        else:
+            if i==lev_1[-1] and winning:
+                print(i)
+                window.fill((225,225,225))
+                time.sleep(5)
+                window.blit(dash,(0,0))
+                pygame.display.update()
+                quitt=True
+                run=False
+            if winning and count<len(lev_1)-1:
+                if len(visited)==len(path[count]):
+                     lives+=1
+                     print(lives)
+                window.fill((0,0,0))
+                x1=0
+                y1=80
+                countx=0
+                county=0
+                winning=False
+                visited=[]
+                discovered=[]
+                back_tracking=False
+                print(visited,path[count])
+                
+                count+=1
+                i=lev_1[count]
+            if lives!=0 and not winning:
+                draw(i)
+                move(i)
+            elif lives==0:
+                window.fill((225,225,225))
+                window.blit(you_lose,(0,0))
+                pygame.display.update()
+    pygame.display.update()
+    
+while quitt and not run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+             quit()
+        else:
+            window.blit(dash,(0,0))
+            pygame.display.update()
+quit()
